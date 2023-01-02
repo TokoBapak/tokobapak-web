cca3bdcb8e7ca9d648a03d841a1c09d6c49512e0
elianiva <dicha.arkana03@gmail.com>
2023-01-03 05:16:06 +0700

    chore: init

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { Title } from "solid-start";
+import Counter from "~/components/Counter";
+
+export default function Home() {
+  return (
+    <main>
+      <Title>Home | Tokobapak</Title>
+      <h1>Tokobapak</h1>
+      <Counter />
+    </main>
+  );
+}
